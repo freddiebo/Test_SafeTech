806a5060e935da5e4a863e29040dbf0406a750e5
VBondarev <freddiebo@yandex.ru>
2020-09-22 12:18:44 +0300

    Initial Commit

---
+++ Test_SafeTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_SafeTech
+//
+//  Created by  Vladislav Bondarev on 22.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
